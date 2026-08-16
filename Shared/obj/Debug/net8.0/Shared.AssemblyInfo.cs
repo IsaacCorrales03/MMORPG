aac59fc9ef942660bc237bd5ab22ef87d59f09ab
Isaac <isaac.corrales0304@gmail.com>
2026-08-16 05:38:11 -0600

    Refactor del git

--- Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47ab7ef470f93e051ae12a6a138dcc6c5e822e70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+181c519e79f7f57f66911ed75c443c2e8ef38514")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
